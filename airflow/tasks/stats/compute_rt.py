2c186a1ccd40e0c60d60389cf5c7bcbd40e4e5fe
jeffbrennan <jeffbrennan10@gmail.com>
2023-03-05 20:56:43 -0500

    implement initial rt calculation in spark

--- airflow/tasks/stats/compute_rt.py
+++ airflow/tasks/stats/compute_rt.py
@@ -1,41 +1,56 @@
 # region imports -----
-import pandas as pd
 import sqlite3
-import pyspark
+import time
+from datetime import timedelta, datetime
+import sqlalchemy
+
+import pandas as pd
 import polars as pl
-from datetime import timedelta
+
+import pyspark
+from pyspark.sql import SparkSession
+# from pyspark.sql.functions import
+
+import random
+import epyestim.covid19 as covid19
 
 
 # endregion
-def load_cases():
-    # TODO: replace with load_cases() after prod db is in complete state
-    # cases = pl.read_sql(
-    #     """
-    #     select County, [Date], Cases_Daily,
-    #     TSA_Combined, PHR_Combined, Metro_Area,
-    #     Population_DSHS
-    #     from county_cases a
-    #     left join county_names b
-    #     on a.County = b.County
-    # """,
-    #     con=conn_prod
-    # )
 
-    cases = (
-        pl.read_csv('https://raw.githubusercontent.com/jeffbrennan/TexasPandemics/master/tableau/county.csv')
-        [['County', 'Date', 'Cases_Daily']]
-    )
 
-    county_names = pl.read_sql('select * from main.county_names', connection_uri=conn_prod)
-    cases_joined = cases.join(county_names, how='inner', on='County')
-
-    # categorical_cols = ['County', 'TSA_Combined', 'PHR_Combined', 'Metro_Area']
+def load_cases(use_disk_file=False):
+    case_cols = ['County', 'Date', 'Cases_Daily']
     num_cols = ['Cases_Daily', 'Population_DSHS']
+
+    # TODO: replace with load_cases() after prod db is in complete state
+    if use_disk_file:
+        cases = pl.read_parquet('county_cases.parquet')[case_cols]
+        county_names = pl.read_sql('select * from main.county_names', connection_uri=conn_prod)
+        cases_joined = cases.join(county_names, how='inner', on='County')
+    else:
+        # cases_joined = pl.read_sql(
+        #     """
+        #     select County, [Date], Cases_Daily,
+        #     TSA_Combined, PHR_Combined, Metro_Area,
+        #     Population_DSHS
+        #     from county_cases a
+        #     left join county_names b
+        #     on a.County = b.County
+        # """,
+        #     con=conn_prod
+        # )
+        county_names = pl.read_sql('select * from main.county_names', connection_uri=conn_prod)
+        case_url = 'https://raw.githubusercontent.com/jeffbrennan/TexasPandemics/master/tableau/county.csv'
+        cases_joined = (
+            pl.read_csv(case_url)
+            [[case_cols]]
+            .join(county_names, how='inner', on='County')
+        )
+
     output_df = (
         cases_joined
         .with_columns(
             [
-                pl.col('Date').str.strptime(pl.Date, '%Y-%m-%d').cast(pl.Date),
                 pl.col(num_cols).cast(pl.UInt32)
             ]
         )
@@ -104,17 +119,82 @@ def prep_rt_groups(cases):
     return rt_prepped_df
 
 
-def calculate_rt():
-    # implement with spark
-    rt_calculated_df = None
-    return rt_calculated_df
+def calculate_rt_spark(pandas_df):
+    case_timeseries = pandas_df.set_index('Date')['Cases_MA_7Day']
+    rt_results_raw = covid19.r_covid(case_timeseries)
+
+    pandas_rt_result = (
+        rt_results_raw
+        .reset_index()
+        .rename(
+            columns=
+            {'index': 'Date',
+             'R_mean': 'rt',
+             'Q0.025': 'ci_low',
+             'Q0.975': 'ci_high'
+             }
+        )
+        .assign(Level=pandas_df['Level'])
+        .assign(Level_Type=pandas_df['Level_Type'])
+        [['Date', 'Level_Type', 'Level', 'rt', 'ci_low', 'ci_high']]
+    )
+    return pandas_rt_result
+
+
+def get_rt(cleaned_cases: pl.DataFrame) -> dict:
+    spark = (
+        SparkSession.builder
+        .appName("covid_rt")
+        .config("spark.memory.offHeap.enabled", "true")
+        .config("spark.memory.offHeap.size", "10g")
+        .getOrCreate()
+    )
+
+    spark_df = spark.createDataFrame(cleaned_cases.to_pandas())[['Date', 'Level_Type', 'Level', 'Cases_MA_7Day']]
+    num_levels = 3
+    # TODO: fix monotonic increasing issue
+    # all_levels = spark_df.select('Level').distinct().toPandas().squeeze().to_list()
+    # sample_levels = random.choices(all_levels, k=num_levels)
+    sample_levels = ['Harris', 'Bexar', 'Travis']
+    spark_df_sample = spark_df.filter(spark_df.Level.isin(sample_levels))
+
+    result_schema = """
+        Date date,
+        Level_Type string,
+        Level string,
+        rt double,
+        ci_low double,
+        ci_high double 
+    """
+
+    start_time = time.time()
+    rt_results = spark_df_sample \
+        .groupBy('Level') \
+        .applyInPandas(calculate_rt_spark, schema=result_schema)
+    rt_results_pandas = rt_results.toPandas()
+    rt_runtime = time.time() - start_time
+    rt_runtime_avg = rt_runtime / num_levels
+
+    output = {
+        'result': rt_results_pandas,
+        'stats': {'runtime': rt_runtime,
+                  'runtime_avg': rt_runtime_avg
+                  }
+    }
+
+    return output
 
 
 def write_results(rt_results):
-    rt_results.to_sql('county_rt', con=conn_prod)
+    # TODO: add metadata and diagnostics
+    # rt_results.spark.to_spark_io(
+    #     format="jdbc", mode="overwrite",
+    #     dbtable="county_rt_test", url=conn_prod
+    # )
+    rt_results['result'].to_sql('county_rt_test', con=conn_prod_pandas, if_exists='replace', index=False)
 
 
-def clean_stacked_cases(stacked_cases):
+def clean_stacked_cases(stacked_cases: pl.DataFrame) -> pl.DataFrame:
     recent_case_avg_df = (
         stacked_cases
         .with_columns(pl.col("Date").max().over("Level").alias("Date_Max"))
@@ -127,21 +207,29 @@ def clean_stacked_cases(stacked_cases):
         )
     )
 
-    cleaned_cases = stacked_cases
-    return cleaned_cases
+    cleaned_cases_df = (
+        stacked_cases
+        .join(recent_case_avg_df, how='inner', on=['Level_Type', 'Level'])
+        .with_columns(pl.col('Cases_Daily').rolling_mean(window_size=7, closed='right').alias('Cases_MA_7day'))
+        .filter(pl.col('Date') >= datetime(2020, 3, 15))
+        [['Date', 'Level_Type', 'Level', 'Cases_MA_7day', 'Population_DSHS']]
+        # .partition_by(['Level_Type', 'Level'], as_dict=True)
+    )
+
+    return cleaned_cases_df
 
 
-def prep_rt(cases):
+def prep_rt(cases: pl.DataFrame) -> dict:
     stacked_cases = prep_rt_groups(cases)
     cleaned_cases = clean_stacked_cases(stacked_cases)
 
     return cleaned_cases
 
-
+conn_prod_pandas = sqlite3.connect('db/prod.db')
 conn_prod = f'sqlite://db/prod.db'
-conn_stage = f'sqlite://db/staging.db'
 
-cases = load_cases()
+
+cases = load_cases(use_disk_file=True)
 cleaned_cases = prep_rt(cases)
-rt_results = calculate_rt(cases_cleaned)
-write_results()
+rt_results = get_rt(cleaned_cases)
+write_results(rt_results)
