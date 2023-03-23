c6adf4b128b37e59e8c95db7093083239479f911
jeffbrennan <jeffbrennan10@gmail.com>
2023-03-22 22:30:37 -0400

    convert spark implementation to polars

--- airflow/tasks/stats/compute_rt_polars.py
+++ airflow/tasks/stats/compute_rt_polars.py
@@ -1,143 +1,96 @@
 # region imports -----
 import sqlite3
 import time
-from datetime import timedelta, datetime
-import sqlalchemy
-
-import pandas as pd
 import polars as pl
-
-import pyspark
-from pyspark.sql import SparkSession
-# from pyspark.sql.functions import
-
-import random
 import epyestim.covid19 as covid19
 
 
 # endregion
-
-
-def load_cases(use_disk_file=False):
-    case_cols = ['County', 'Date', 'Cases_Daily']
-    num_cols = ['Cases_Daily', 'Population_DSHS']
-
-    # TODO: replace with load_cases() after prod db is in complete state
-    if use_disk_file:
-        cases = pl.read_parquet('county_cases.parquet')[case_cols]
-        county_names = pl.read_sql('select * from main.county_names', connection_uri=conn_prod)
-        cases_joined = cases.join(county_names, how='inner', on='County')
-    else:
-        # cases_joined = pl.read_sql(
-        #     """
-        #     select County, [Date], Cases_Daily,
-        #     TSA_Combined, PHR_Combined, Metro_Area,
-        #     Population_DSHS
-        #     from county_cases a
-        #     left join county_names b
-        #     on a.County = b.County
-        # """,
-        #     con=conn_prod
-        # )
-        county_names = pl.read_sql('select * from main.county_names', connection_uri=conn_prod)
-        case_url = 'https://raw.githubusercontent.com/jeffbrennan/TexasPandemics/master/tableau/county.csv'
-        cases_joined = (
-            pl.read_csv(case_url)
-            [[case_cols]]
-            .join(county_names, how='inner', on='County')
-        )
-
-    output_df = (
-        cases_joined
-        .with_columns(
-            [
-                pl.col(num_cols).cast(pl.UInt32)
-            ]
+def write_results(rt_results):
+    (
+        rt_results['result']
+        .to_pandas()
+        .to_sql(
+            'county_rt_test_polars',
+            con=conn_prod_pandas,
+            if_exists='replace',
+            index=False
         )
     )
 
-    return output_df
+
+def get_case_timeseries(cases: pl.Series, dates: pl.Series) -> pl.Series:
+    return cases.to_pandas().set_axis(dates.to_pandas())
 
 
+def calculate_rt(df: pl.DataFrame) -> pl.DataFrame:
+    print(df.get_column('Level')[0])
 
-def calculate_rt_spark(pandas_df):
-    case_timeseries = pandas_df.set_index('Date')['Cases_MA_7Day']
+    case_timeseries = get_case_timeseries(df.get_column('Cases_MA_7day'), df.get_column('Date'))
     rt_results_raw = covid19.r_covid(case_timeseries)
 
-    pandas_rt_result = (
-        rt_results_raw
-        .reset_index()
-        .rename(
-            columns=
-            {'index': 'Date',
-             'R_mean': 'rt',
-             'Q0.025': 'ci_low',
-             'Q0.975': 'ci_high'
-             }
-        )
-        .assign(Level=pandas_df['Level'])
-        .assign(Level_Type=pandas_df['Level_Type'])
-        [['Date', 'Level_Type', 'Level', 'rt', 'ci_low', 'ci_high']]
+    polars_rt_result = (
+        pl.from_pandas(rt_results_raw.reset_index())
+        .rename({'index': 'Date',
+                 'R_mean': 'rt',
+                 'Q0.025': 'ci_low',
+                 'Q0.975': 'ci_high'
+                 }
+                )
+        [['Date', 'rt', 'ci_low', 'ci_high']]
     )
-    return pandas_rt_result
+
+    final_result = (
+        df.
+        join(other=polars_rt_result, on='Date', how='left')
+    )
+
+    return final_result
 
 
 def get_rt(cleaned_cases: pl.DataFrame) -> dict:
-    spark = (
-        SparkSession.builder
-        .appName("covid_rt")
-        .config("spark.memory.offHeap.enabled", "true")
-        .config("spark.memory.offHeap.size", "10g")
-        .getOrCreate()
+    sample_levels = ['Harris', 'Bexar', 'Travis']
+    num_levels = len(sample_levels)
+
+    cases_df_sample = (
+        cleaned_cases
+        .filter(pl.col('Level').is_in(sample_levels))
     )
 
-    spark_df = spark.createDataFrame(cleaned_cases.to_pandas())[['Date', 'Level_Type', 'Level', 'Cases_MA_7Day']]
-    num_levels = 3
-    # TODO: fix monotonic increasing issue
-    # all_levels = spark_df.select('Level').distinct().toPandas().squeeze().to_list()
-    # sample_levels = random.choices(all_levels, k=num_levels)
-    sample_levels = ['Harris', 'Bexar', 'Travis']
-    spark_df_sample = spark_df.filter(spark_df.Level.isin(sample_levels))
-
-    result_schema = """
-        Date date,
-        Level_Type string,
-        Level string,
-        rt double,
-        ci_low double,
-        ci_high double 
-    """
+    group_split_cases = cases_df_sample.partition_by(by=['Level_Type', 'Level'], maintain_order=True)
 
     start_time = time.time()
-    rt_results = spark_df_sample \
-        .groupBy('Level') \
-        .applyInPandas(calculate_rt_spark, schema=result_schema)
-    rt_results_pandas = rt_results.toPandas()
+    rt_results = pl.concat([calculate_rt(i) for i in group_split_cases])
+
     rt_runtime = time.time() - start_time
     rt_runtime_avg = rt_runtime / num_levels
 
     output = {
-        'result': rt_results_pandas,
+        'result': rt_results,
         'stats': {'runtime': rt_runtime,
                   'runtime_avg': rt_runtime_avg
                   }
     }
-
     return output
 
 
-def write_results(rt_results):
-    # TODO: add metadata and diagnostics
-    # rt_results.spark.to_spark_io(
-    #     format="jdbc", mode="overwrite",
-    #     dbtable="county_rt_test", url=conn_prod
-    # )
-    rt_results['result'].to_sql('county_rt_test', con=conn_prod_pandas, if_exists='replace', index=False)
+def load_cases():
+    df_raw = pl.read_sql(sql='select * from covid_rt_prep', connection_uri=conn_prod)
+
+    df = (
+        df_raw
+        .with_columns(
+            [
+                pl.col('Population_DSHS').cast(pl.UInt32)
+            ]
+        )
+    )
+
+    return df
 
 
-conn_prod_pandas = sqlite3.connect('db/prod.db')
 conn_prod = f'sqlite://db/prod.db'
-
+conn_prod_pandas = sqlite3.connect('db/prod.db')
 
 cleaned_cases = load_cases()
 rt_results = get_rt(cleaned_cases)
