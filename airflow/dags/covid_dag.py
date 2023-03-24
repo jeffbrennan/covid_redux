969b0abb435efc2a6bfc0c6f47282cc56039496e
jeffbrennan <jeffbrennan10@gmail.com>
2023-03-24 00:16:33 -0400

    get dag working using initial case/death pull

--- airflow/dags/covid_dag.py
+++ airflow/dags/covid_dag.py
@@ -1,70 +1,52 @@
-#region Imports -----
-
-
-#region airflow -----
-
 from airflow import DAG
-from random import randint
-
-from airflow.operators.python import PythonOperator, BranchPythonOperator
-from airflow.operators.bash import BashOperator
-#endregion
-
-#region general -----
-import pandas as pd
-import re
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
 from datetime import datetime as dt
-from datetime import timedelta
+from tasks.extract.get_covid_vitals_daily import get_vitals, write_db
 
 
-import os
-
-#endregion
-
-#endregion
-
-#region globals -----
-if dt.utcnow().hour > 4 and dt.utcnow().hour < 16:
-    TODAY = dt.now() - timedelta(days=1)
-else:
-    TODAY = dt.now()
-
-
-os.chdir('C:/users/jeffb/Desktop/Life/personal-projects/de/covid_redux')
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'catchup': False,
+    'start_date': dt(2023, 3, 23),
+    'email': ['jeffbrennan10@gmail.com'],
+    'retries': 0,
+}
 
 with DAG(
-        "my_dag",
-        start_date=dt.datetime(2021,1,1),
-        schedule_interval="@daily",
-        catchup=False
+        dag_id='covid_dag',
+        schedule_interval='@weekly',
+        default_args=default_args
 ) as dag:
-    # run 3 models that select number between 1 and 10
-    get_county_data = PythonOperator(
-        task_id="get_county_vitals",
-        python_callable=get_county_data  #calls python function _training_model()
+    get_case_task = PythonOperator(
+        task_id='get_cases',
+        python_callable=get_vitals,
+        op_kwargs={
+            'url': Variable.get('dshs_case_url'),
+            'file_name': 'raw_cases_daily'}
     )
 
-    get_county_vitals = PythonOperator(
-        task_id="get_county_vitals",
-        python_callable=get_county_vitals  # calls python function _training_model()
+    get_death_task = PythonOperator(
+        task_id='get_deaths',
+        python_callable=get_vitals,
+        op_kwargs={
+            'url': Variable.get('dshs_death_url'),
+            'file_name': 'raw_deaths_daily'
+        }
     )
 
-    write_db = PythonOperator(
-        task_id="upload_county_vitals",
-        python_callable=write_db  # calls python function _training_model()
+    write_db_cases = PythonOperator(
+        task_id='write_db_cases',
+        python_callable=write_db,
+        op_kwargs={'df_name': 'raw_cases_daily', 'table_name': 'county_vitals_cases'}
     )
 
-   #  run_statistics = PythonOperator(
-   #      task_id="run_statistics",
-   #      python_callable=_training_model  # calls python function _training_model()
-   #  )
-   #
-   #  upload_statistics = PythonOperator(
-   #      task_id="run_statistics",
-   #      python_callable=_training_model  # calls python function _training_model()
-   # )
+    write_db_deaths = PythonOperator(
+        task_id='write_db_deaths',
+        python_callable=write_db,
+        op_kwargs={'df_name': 'raw_deaths_daily', 'table_name': 'county_vitals_deaths'}
+    )
 
-
-
-   [get_county_data] >> [get_county_vitals] >> [write_db]
-   # >> [run_statistics] >> [upload_statistics]
+    get_case_task >> write_db_cases
+    get_death_task >> write_db_deaths
