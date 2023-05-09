54481b1272fe18e6560162d68a90538b0e908502
jeffbrennan <jeffbrennan10@gmail.com>
2023-05-09 10:00:15 -0400

    add key names

--- covid_pipeline/covid_dagster/assets/__init__.py
+++ covid_pipeline/covid_dagster/assets/__init__.py
@@ -13,6 +13,6 @@ DBT_PROFILES = file_relative_path(__file__, "../../covid_dbt/config")
 dbt_assets = load_assets_from_dbt_project(
     project_dir=DBT_PROJECT_PATH,
     profiles_dir=DBT_PROFILES,
-    key_prefix=["dbt"],
-    io_manager_key="dbt_to_dbt_io_manager",
+    key_prefix=["covid"],
+    io_manager_key="dbt_io_manager",
 )
